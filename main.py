3c8fc5e3bc3a7a6791050d9d0dffbc7bfa292953
Aayush Adhikari <77236863+Aayush518@users.noreply.github.com>
2023-08-12 20:59:34 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,47 @@
-import numpy as np
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.manifold import TSNE
-from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Load the dataset from the csv file
 data = pd.read_csv('creditcard.csv')
 
-# Understand the structure of the data
-print(data.columns)
-print(data.shape)
-print(data.describe())
-
-# Check for missing values
-print(data.isnull().sum())
-
-# Look at the class distribution for fraud vs. non-fraud transactions
-print(data['Class'].value_counts())
-
-# Since most of our data has already been scaled we should scale the columns that are left to scale (Amount and Time)
 std_scaler = StandardScaler()
-
-data['scaled_amount'] = std_scaler.fit_transform(data['Amount'].values.reshape(-1,1))
-data['scaled_time'] = std_scaler.fit_transform(data['Time'].values.reshape(-1,1))
-
-# Now drop the original columns (Amount and Time)
-data = data.drop(['Time','Amount'], axis=1)
-
-# Define the feature set and target variable
+data['scaled_amount'] = std_scaler.fit_transform(data['Amount'].values.reshape(-1, 1))
+data['scaled_time'] = std_scaler.fit_transform(data['Time'].values.reshape(-1, 1))
+data = data.drop(['Time', 'Amount'], axis=1)
 X = data.drop('Class', axis=1)
 y = data['Class']
-
-# Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-# Instantiate the model (using the default parameters)
 logreg = LogisticRegression()
-
-# Fit the model with data
 logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
 
-# Predict on the test data
-y_pred=logreg.predict(X_test)
+def main():
+    st.title("Credit Card Fraud Detection")
+    st.sidebar.header("User Input Features")
 
-# Check the accuracy of the model
-print("Accuracy:", accuracy_score(y_test, y_pred))
+    user_input = {}
 
-# Check the classification report
-print(classification_report(y_test, y_pred))
+    user_input['V1'] = st.sidebar.number_input("V1", value=0.0)
+    user_input['V2'] = st.sidebar.number_input("V2", value=0.0)
 
-# Confusion matrix
-cnf_matrix = confusion_matrix(y_test, y_pred.round())
-sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu" ,fmt='g')
-plt.tight_layout()
-plt.title('Confusion matrix')
-plt.ylabel('Actual label')
-plt.xlabel('Predicted label')
+
+    if st.sidebar.button("Predict"):
+        prediction = logreg.predict([list(user_input.values())])
+        st.write("Prediction:", prediction)
+
+    st.subheader("Confusion Matrix")
+    cnf_matrix = confusion_matrix(y_test, y_pred.round())
+    fig, ax = plt.subplots()
+    sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu", fmt='g', ax=ax)
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
