1e96bf65b9902dfad5455208e200127fb7ed9142
Aayush Adhikari <77236863+Aayush518@users.noreply.github.com>
2023-08-15 21:57:57 +0545

    Update main.py

--- main.py
+++ main.py
@@ -12,30 +12,42 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('creditcard.csv')
 
 # Preprocess the data
-std_scaler = StandardScaler()
-data['scaled_amount'] = std_scaler.fit_transform(data['Amount'].values.reshape(-1, 1))
-data['scaled_time'] = std_scaler.fit_transform(data['Time'].values.reshape(-1, 1))
-data = data.drop(['Time', 'Amount'], axis=1)
-X = data.drop('Class', axis=1)
-y = data['Class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+def preprocess_data(data):
+    std_scaler = StandardScaler()
+    data['scaled_amount'] = std_scaler.fit_transform(data['Amount'].values.reshape(-1, 1))
+    data['scaled_time'] = std_scaler.fit_transform(data['Time'].values.reshape(-1, 1))
+    data = data.drop(['Time', 'Amount'], axis=1)
+    return data
 
-# Train the logistic regression model
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
+def train_model(data):
+    X = data.drop('Class', axis=1)
+    y = data['Class']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# Streamlit app
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    y_pred = logreg.predict(X_test)
+
+    return logreg, X_test, y_test, y_pred
+
 def main():
     st.title("Credit Card Fraud Detection")
     st.sidebar.header("User Input Features")
 
     # Collect user input for features
     user_input = {}
-    user_input['V1'] = st.sidebar.number_input("V1", value=0.0)
-    user_input['V2'] = st.sidebar.number_input("V2", value=0.0)
-    user_input['V3'] = st.sidebar.number_input("V3", value=0.0)
-    user_input['V4'] = st.sidebar.number_input("V4", value=0.0)
+    for col in ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9']:
+        user_input[col] = st.sidebar.number_input(col, value=0.0)
+
+    # Load and preprocess the data
+    processed_data = preprocess_data(data.copy())
+
+    # Add new features to the processed data
+    for col, value in user_input.items():
+        processed_data[col] = value
+
+    # Train the logistic regression model
+    logreg, X_test, y_test, y_pred = train_model(processed_data)
 
     # Perform prediction on user input
     if st.sidebar.button("Predict"):
@@ -50,6 +62,16 @@ def main():
         plt.xlabel("Predicted")
         plt.ylabel("Actual")
         st.pyplot(fig)
+
+    # Display distribution of features
+    st.sidebar.subheader("Feature Distributions")
+    for col in user_input.keys():
+        st.sidebar.write(f"**{col}**")
+        plt.figure(figsize=(6, 4))
+        sns.histplot(processed_data[col], bins=30, kde=True)
+        plt.xlabel(col)
+        plt.ylabel("Density")
+        st.sidebar.pyplot(plt)
 
 if __name__ == '__main__':
     main()
